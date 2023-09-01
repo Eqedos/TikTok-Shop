67690968f39cbd5bd61741fe650391fb819e31bb
Parth <parthkumarj117@@gmail.com>
2023-09-01 20:18:59 +0800

    init product

--- App.tsx
+++ App.tsx
@@ -1,17 +1,54 @@
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { User, onAuthStateChanged } from 'firebase/auth';
+
 import Login from './app/screens/Login';
+import Products from './app/screens/Products';
+import { FIREBASE_AUTH } from './FirebaseConfig';
 
 const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+  const [initializing, setInitializing] = useState(true); // To handle the intermediate state
+
+  useEffect(() => {
+      const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, user => {
+          if (user) {
+              setUser(user);
+          } else {
+              setUser(null);
+          }
+          if (initializing) setInitializing(false); // Done initializing after the first check
+      });
+
+      // Cleanup subscription on unmount
+      return unsubscribe;
+  }, [initializing]);
+
+  if (initializing) {
+      return null; // or a loader if you want to show something during initialization
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-       <Stack.Screen name='Login' component={Login} options={ {headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+          {user ? (
+              <InsideStack.Navigator initialRouteName="products">
+                  <InsideStack.Screen name="products" component={Products} />
+              </InsideStack.Navigator>
+          ) : (
+              <Stack.Navigator initialRouteName="Login">
+                  <Stack.Screen 
+                      name="Login" 
+                      component={Login} 
+                      options={{ headerShown: false }}
+                  />
+              </Stack.Navigator>
+          )}
+      </NavigationContainer>
   );
 }
+
--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAqWICxOv-nsF3ndm4Q-cS2gOVE4Lx7Rr0",
   authDomain: "tiktokshop-f366c.firebaseapp.com",
@@ -16,7 +12,5 @@ const firebaseConfig = {
   measurementId: "G-MFNMNQJ014"
 };
 
-// Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-
