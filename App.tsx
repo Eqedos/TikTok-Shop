ba0ce5b51d640f30504284f77246f2dc0dfaca48
Parth <parthkumarj117@@gmail.com>
2023-09-08 23:51:14 +0800

    ui changes

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProductDetailsForm from './app/screens/ProductTabs/ProductDetailsForm'; // Adjust the path as necessary
 import { User, onAuthStateChanged } from 'firebase/auth';
+import { StatusBar, Platform } from 'react-native';
 
 import Login from './app/screens/Login';
 import Products from './app/screens/Products';
@@ -10,6 +11,8 @@ import ProductInfo from './app/screens/ProductInfo'; // Make sure to create this
 import { FIREBASE_AUTH } from './FirebaseConfig';
 import TikTokForm from './app/screens/ProductTabs/TikTokForm';
 import ReviewForm from './app/screens/ProductTabs/ReviewForm'; 
+import { lockAsync, OrientationLock } from 'expo-screen-orientation';
+import Constants from 'expo-constants';
 
 
 export type RootStackParamList = {
@@ -29,6 +32,13 @@ export default function App() {
   const [initializing, setInitializing] = useState(true); // To handle the intermediate state
 
   useEffect(() => {
+    async function changeScreenOrientation() {
+        if (Platform.OS === 'android' && !Constants.isDevice) { // Ensure it's on an Android device
+          await lockAsync(OrientationLock.ALL);
+          StatusBar.setHidden(true);
+        }
+      }
+      changeScreenOrientation();
       const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, user => {
           console.log("User state changed: ", user);
           if (user) {
@@ -49,11 +59,15 @@ export default function App() {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName={user ? "Products" : "Login"}>
+    <Stack.Navigator initialRouteName={user ? "Products" : "Login"} screenOptions={{
+    headerShown: false,
+    statusBarHidden: true,  // Add this line
+  }}>
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        
         {user ? (
             <React.Fragment>
-                <Stack.Screen name="Products" component={Products} options={{ headerShown: false }}/>
+                <Stack.Screen name="Products" component={Products} options={{ headerShown: false } }/>
                 <Stack.Screen name="ProductInfo" component={ProductInfo} options={{ headerShown: false }}/>
                 <Stack.Screen name="ProductDetailsForm" component={ProductDetailsForm} options={{ headerShown: false }}/>
                 <Stack.Screen name="TikTokForm" component={TikTokForm} options={{ headerShown: false }}/>
