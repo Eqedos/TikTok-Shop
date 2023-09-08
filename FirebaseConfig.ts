bccc07d0c74292567529c33913c6ae6b96eb3819
Parth <parthkumarj117@@gmail.com>
2023-09-08 23:35:15 +0800

    add nav bar

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,13 +5,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqWICxOv-nsF3ndm4Q-cS2gOVE4Lx7Rr0",
-  authDomain: "tiktokshop-f366c.firebaseapp.com",
-  projectId: "tiktokshop-f366c",
-  storageBucket: "tiktokshop-f366c.appspot.com",
-  messagingSenderId: "868478067977",
-  appId: "1:868478067977:web:fb982e1ea4ea1bc35a9fd8",
-  measurementId: "G-MFNMNQJ014"
+  apiKey: "AIzaSyB2sResCqLl4jg4Blv2KBHrB4pnhG99e84",
+  authDomain: "tiktokshop2-61a3f.firebaseapp.com",
+  projectId: "tiktokshop2-61a3f",
+  storageBucket: "tiktokshop2-61a3f.appspot.com",
+  messagingSenderId: "937531028712",
+  appId: "1:937531028712:web:4a56fa72eda4124408455d"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
--- app/screens/ProductInfo.tsx
+++ app/screens/ProductInfo.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { useRoute, RouteProp } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import ProductDetails from './ProductTabs/ProductDetails';
 import Reviews from './ProductTabs/Reviews';
 import TikTok from './ProductTabs/TikTok';
@@ -10,7 +11,17 @@ import { RootStackParamList } from '../../App';
 
 type ProductInfoRouteProp = RouteProp<RootStackParamList, 'ProductInfo'>;
 
-const Tab = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
+
+import Svg, { Path } from 'react-native-svg';
+
+const TikTokIcon = ({ color = "#000000" }) => {
+    return (
+        <Svg width="30" height="30" viewBox="0 0 50 50">
+            <Path fill={color} d="M41,4H9C6.243,4,4,6.243,4,9v32c0,2.757,2.243,5,5,5h32c2.757,0,5-2.243,5-5V9C46,6.243,43.757,4,41,4z M37.006,22.323 c-0.227,0.021-0.457,0.035-0.69,0.035c-2.623,0-4.928-1.349-6.269-3.388c0,5.349,0,11.435,0,11.537c0,4.709-3.818,8.527-8.527,8.527 s-8.527-3.818-8.527-8.527s3.818-8.527,8.527-8.527c0.178,0,0.352,0.016,0.527,0.027v4.202c-0.175-0.021-0.347-0.053-0.527-0.053 c-2.404,0-4.352,1.948-4.352,4.352s1.948,4.352,4.352,4.352s4.527-1.894,4.527-4.298c0-0.095,0.042-19.594,0.042-19.594h4.016 c0.378,3.591,3.277,6.425,6.901,6.685V22.323z" />
+        </Svg>
+    );
+};
 
 const ProductInfo: React.FC = () => {
   const route = useRoute<ProductInfoRouteProp>();
@@ -19,12 +30,28 @@ const ProductInfo: React.FC = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: 'tomato',
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Details') {
+              iconName = 'info-circle';
+            } else if (route.name === 'Reviews') {
+              iconName = 'star';
+            } else if (route.name === 'TikTok') {
+              return <TikTokIcon color={color} />;
+            } else {
+              iconName=''
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'white',
           tabBarInactiveTintColor: 'gray',
           tabBarLabelStyle: { fontSize: 12 },
-          tabBarStyle: { backgroundColor: '#e6e6e6' },
-        }}
+          tabBarStyle: { backgroundColor: 'black' },
+          headerShown: false,
+        })}
       >
         <Tab.Screen 
           name="Details" 
