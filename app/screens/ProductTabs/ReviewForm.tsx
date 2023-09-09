22b8cf9dc63db28d5dc4f9dd735812f930b4ff44
Parth <parthkumarj117@@gmail.com>
2023-09-09 08:00:10 +0800

    add review and likes workes

--- app/screens/ProductTabs/ReviewForm.tsx
+++ app/screens/ProductTabs/ReviewForm.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import { ref, push } from "firebase/database";
+import { ref, push, update, set } from "firebase/database";
 import { FIREBASE_DB } from '../../../FirebaseConfig';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../App';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type ReviewFormProps = NativeStackScreenProps<RootStackParamList, 'ReviewForm'>;
 
 const ReviewForm: React.FC<ReviewFormProps> = (props) => {
   const [author, setAuthor] = useState<string>('');
   const [text, setText] = useState<string>('');
+  const [rating, setRating] = useState<number>(0);
 
   const productName = props.route.params.productName;
 
@@ -19,13 +21,47 @@ const ReviewForm: React.FC<ReviewFormProps> = (props) => {
       return;
     }
 
+    const reviewsRef = ref(FIREBASE_DB, `products/${productName}/reviews`);
+    
+    // Generate a new unique key
+    const newKey = push(reviewsRef).key;
+
+    // If the new key is null (although unlikely), handle this error gracefully
+    if (!newKey) {
+      console.error('Failed to generate a new key for the review.');
+      return;
+    }
+
+    const newReviewRef = ref(FIREBASE_DB, `products/${productName}/reviews/${newKey}`);
+
     const reviewData = {
+      id: newKey,
       author,
-      text
+      text,
+      rating,
+      likes: 0
     };
 
-    const reviewsRef = ref(FIREBASE_DB, `products/${productName}/reviews`);
-    push(reviewsRef, reviewData);
+    set(newReviewRef, reviewData); // Use set to add the data to the reference
+};
+
+  
+  
+
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Icon
+          key={i}
+          name={i <= rating ? 'star' : 'star-o'}
+          size={30}
+          color={i <= rating ? 'gold' : 'gray'}
+          onPress={() => setRating(i)}
+        />
+      );
+    }
+    return stars;
   };
 
   return (
@@ -48,6 +84,11 @@ const ReviewForm: React.FC<ReviewFormProps> = (props) => {
         value={text}
       />
 
+      <View style={styles.ratingContainer}>
+        <Text>Rating:</Text>
+        {renderStars()}
+      </View>
+
       <Button title="Submit Review" onPress={submitReview} />
     </View>
   );
@@ -68,6 +109,11 @@ const styles = StyleSheet.create({
   textArea: {
     height: 100,
     textAlignVertical: 'top'
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
   }
 });
 
--- app/screens/ProductTabs/Reviews.tsx
+++ app/screens/ProductTabs/Reviews.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../../App'; // Adjust this path according to your project structure.
+import { RootStackParamList } from '../../../App';
+import { ref as sref, onValue, update, DatabaseReference } from "firebase/database";
+import { FIREBASE_DB } from '../../../FirebaseConfig';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type ReviewsProps = {
   productName: string;
@@ -12,17 +15,141 @@ type ReviewsNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Revi
 
 const Reviews: React.FC<ReviewsProps> = ({ productName }) => {
   const navigation = useNavigation<ReviewsNavigationProp>();
+  const [reviews, setReviews] = useState<any[]>([]);
+
+  useEffect(() => {
+    const reviewsRef = sref(FIREBASE_DB, `products/${productName}/reviews`);
+    onValue(reviewsRef, (snapshot) => {
+      const reviewsData = snapshot.val();
+      if (reviewsData) {
+        const reviewsArray = Object.values(reviewsData);
+        setReviews(reviewsArray);
+      }
+    });
+  }, [productName]);
 
   const navigateToReviewForm = () => {
     navigation.navigate('ReviewForm', { productName });
   };
 
+  const handleLike = (review: any) => {
+    const reviewRef = sref(FIREBASE_DB, `products/${productName}/reviews/${review.id}`); // Use id instead of key
+    const updatedLikes = review.likes + 1;
+  
+    // Use the 'update' method to update the likes count
+    update(reviewRef, { likes: updatedLikes })
+      .then(() => {
+        // Update the local state with the updated likes count
+        const updatedReviews = reviews.map((r) => {
+          if (r.id === review.id) {  // Use id instead of key
+            return { ...r, likes: updatedLikes };
+          }
+          return r;
+        });
+        setReviews(updatedReviews);
+      })
+      .catch((error) => {
+        console.log('Error updating likes:', error);
+      });
+};
+  
+  
+
+  const renderStars = (rating: number) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Icon
+          key={i}
+          name={i <= rating ? 'star' : 'star-o'}
+          size={20}
+          color={i <= rating ? 'gold' : 'gray'}
+        />
+      );
+    }
+    return stars;
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Reviews for {productName}</Text>
-      <Button title="Add Review" onPress={navigateToReviewForm} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Reviews for {productName}</Text>
+      </View>
+      <View style={styles.content}>
+        {reviews.map((review, index) => (
+          <View key={index} style={styles.reviewContainer}>
+            <View style={styles.reviewBox}>
+              <View style={styles.reviewTextContainer}>
+                <Text style={styles.authorText}>Author: {review.author}</Text>
+                <Text style={styles.ratingText}>Rating: {renderStars(review.rating)}</Text>
+                <Text style={styles.reviewText}>Review: {review.text}</Text>
+              </View>
+              <TouchableOpacity style={styles.likeButton} onPress={() => handleLike(review)}>
+                <Icon name="thumbs-up" size={20} color="gray" />
+                <Text style={styles.likeCount}>{review.likes}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
+        <Button title="Add Review" onPress={navigateToReviewForm} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 20,
+  },
+  header: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  reviewContainer: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  reviewBox: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  reviewTextContainer: {
+    flex: 1,
+  },
+  authorText: {
+    fontSize: 16,
+  },
+  ratingText: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  reviewText: {
+    fontSize: 14,
+  },
+  likeButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  likeCount: {
+    marginLeft: 5,
+  },
+});
 
 export default Reviews;
